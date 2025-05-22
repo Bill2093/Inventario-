3200503dc727accaeb6d1494884510b6a2a6523f
Bill2093 <pedroluisoa20@gmail.com>
2025-05-22 00:26:45 -0500

    Propiedades de Backend

---
+++ Inventario.Backend/Models/Producto.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Inventario.Backend.Models
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+
+        public string Nombre { get; set; } = null!; // el copilador no marque error con el string 
+
+        public int Cantidad { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]  // el atributo columna con la cantidad de caracteres y decimales
+        [DisplayFormat(DataFormatString = "{0:c2}")] // atributo para ver el precio
+
+        public decimal Precio { get; set; }
+
+        public int Stock {get; set; }
+
+    }
+
+}
