53649bf1d47ca9d7d47d3f23819b081e8700092d
Bill2093 <pedroluisoa20@gmail.com>
2025-05-22 02:27:57 -0500

    conexion a base de datos, Creacion e de la clase operaciones

---
+++ Inventario.Backend/Models/Operacion.cs
@@ -0,0 +1,13 @@
+﻿namespace Inventario.Backend.Models
+{
+    public class Operacion
+    {
+        public int Id { get; set; }
+        public string Tipo { get; set; } = null!;
+        public string Producto { get; set; } = null!;
+        public DateTime Fecha { get; set; }
+        public int Cantidad { get; set; }
+        public int Stock { get; set; }
+
+    }
+}
--- Inventario.Backend/Program.cs
+++ Inventario.Backend/Program.cs
@@ -1,25 +1,41 @@
-var builder = WebApplication.CreateBuilder(args);
+using Inventario.Backend.Models; //hacer el llamado del archivo 
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
-// Add services to the container.
+public class Program
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public static void Main(string[] args)
+    {
+
+        var builder = WebApplication.CreateBuilder(args);
+        var configuration = builder.Configuration;
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+        builder.Services.AddDbContext<DataContext>(options =>
+        options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"))); // conexion de base datos
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle 
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
